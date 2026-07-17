2a4c6eed62f82ba2f4ca145d0a51b641d1ba06ad
AldJayR <aldwinjzs3@gmail.com>
2026-07-17 18:56:31 +0800

    added frontend deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
@@ -6,6 +7,7 @@ import { defineConfig } from "vitest/config";
 
 const config = defineConfig({
 	plugins: [
+		cloudflare({ viteEnvironment: { name: "ssr" } }),
 		devtools(),
 		tailwindcss(),
 		tanstackStart(),
